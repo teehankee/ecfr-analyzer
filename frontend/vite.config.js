629f65d4b36c1e9f411e2c0f1f21f8df8f6f63f2
Dylan Teehankee <57874550+teehankee@users.noreply.github.com>
2025-05-16 12:22:47 -0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/ecfr-analyzer/',
   server: {
     proxy: {
       "/api": "http://localhost:8000",
